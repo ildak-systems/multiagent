ba50b68780c94179d393c3a850558ee9763a23ec
ildak-systems <ildak@uw.edu>
2024-02-23 02:00:28 -0800

    Q1 passed, 4 points

--- multiAgents.py
+++ multiAgents.py
@@ -18,6 +18,7 @@ import random, util
 
 from game import Agent
 
+
 class ReflexAgent(Agent):
     """
     A reflex agent chooses an action at each choice point by examining
@@ -28,7 +29,6 @@ class ReflexAgent(Agent):
     headers.
     """
 
-
     def getAction(self, gameState):
         """
         You do not need to change this method, but you're welcome to.
@@ -37,15 +37,25 @@ class ReflexAgent(Agent):
 
         Just like in the previous project, getAction takes a GameState and returns
         some Directions.X for some X in the set {NORTH, SOUTH, WEST, EAST, STOP}
+
+        An unimplemented ReflexAgent is driven by getting all the legal moves from the
+        current position, getting a potential future score from each of those moves,
+        (which move will vary with different score by food, no food, presence of ghost, etc.)
+        and return the score for each moves. getAction() simply picks the move with the highest
+        score. It does not consider any future impacts from choosing the action, just the
+        score from taking the immediate action.
+
         """
         # Collect legal moves and successor states
         legalMoves = gameState.getLegalActions()
 
         # Choose one of the best actions
+        # unimplemented version simply gets the score not considering the ghost, food,
+        # scared ghost, etc.
         scores = [self.evaluationFunction(gameState, action) for action in legalMoves]
         bestScore = max(scores)
         bestIndices = [index for index in range(len(scores)) if scores[index] == bestScore]
-        chosenIndex = random.choice(bestIndices) # Pick randomly among the best
+        chosenIndex = random.choice(bestIndices)  # Pick randomly among the best
 
         "Add more of your code here if you want to"
 
@@ -65,16 +75,95 @@ class ReflexAgent(Agent):
 
         Print out these variables to see what you're getting, then combine them
         to create a masterful evaluation function.
-        """
-        # Useful information you can extract from a GameState (pacman.py)
-        successorGameState = currentGameState.generatePacmanSuccessor(action)
-        newPos = successorGameState.getPacmanPosition()
-        newFood = successorGameState.getFood()
-        newGhostStates = successorGameState.getGhostStates()
-        newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
 
-        "*** YOUR CODE HERE ***"
-        return successorGameState.getScore()
+        Without a valid evaluation function, I noticed that when pacman is not
+        surrounded by food, it has trouble finding a path and goes back and forth
+        since every action gives the same "best" score, and it picks it randomly.
+        Giving the illusion that its moving back and forth with no sense of route.
+
+        .getFood():
+        Returns a Grid of boolean food indicator variables.
+
+        Grids can be accessed via list notation, so to check
+        if there is food at (x,y), just call
+
+        currentFood = state.getFood()
+        if currentFood[x][y] == True: ...
+
+        the action parameter is the successor move from the current state
+
+        This function gives the score from taking the "action" move from
+        the currrent state by evaluating the current game env
+        """
+
+        score = 100
+
+        # successorGameState: Give the state as a grid from taking the action
+        # use successorGameState.getScore() to get the immediate score
+        successorGameState = currentGameState.generatePacmanSuccessor(action)
+        newSuccessorPosition = successorGameState.getPacmanPosition()
+        pacmanPosition = currentGameState.getPacmanPosition()
+
+        # Pacman does not know what to do if there are no food pallets adjacent to him
+        # Return .getScore() if food is directly adjacent him
+        # If not, give Manhattan distance to the closest food pallet
+
+        capsules = currentGameState.getCapsules()
+
+        hasFood = currentGameState.getFood()
+        x = pacmanPosition[0]
+        y = pacmanPosition[1]
+        if newSuccessorPosition in capsules:
+            return 10000
+        elif hasFood[x + 1][y]:
+            return score + successorGameState.getScore()
+
+        elif hasFood[x][y + 1]:
+            return score + successorGameState.getScore()
+
+        elif hasFood[x - 1][y]:
+            return score + successorGameState.getScore()
+        elif hasFood[x][y - 1]:
+            return score + successorGameState.getScore()
+        else:
+            # No more adjacent food! Find the closest food
+            closestFood = (float('inf'), float('inf'))
+            minDist = float('inf')
+            for x in range(24):
+                for y in range(len(hasFood[x])):
+                    if hasFood[x][y]:
+                        manDist = manhattanDistance(pacmanPosition, (x, y))
+                        if manDist < minDist:
+                            minDist = manDist
+                            closestFood = (x, y)
+
+            # smaller the distance the better
+            # reward if taking the new position got us closer to the closest food
+            # penalize if opposite
+            prevDistance = manhattanDistance(pacmanPosition, closestFood)
+            distance = manhattanDistance(newSuccessorPosition, closestFood)
+
+            if prevDistance < distance:
+                score = score - 100
+            elif distance < prevDistance:
+                score = score + 100
+            else:
+                score = score - 10
+
+            # decrement if action is Stop
+            if action == "Stop":
+                score = score - 100
+
+            # Pacman should move towards food pallet but according to the distance between the ghost
+            ghostPosition = successorGameState.getGhostPositions()[0]
+
+            # I want to motivate Pacman to eat more food, not be scared about ghosts
+            # so only penalize a move if it's way too close to the ghost
+            distGhost = manhattanDistance(ghostPosition, newSuccessorPosition)
+            if distGhost < 2:
+                score = score - 500
+
+            return score
 
 def scoreEvaluationFunction(currentGameState):
     """
@@ -85,6 +174,7 @@ def scoreEvaluationFunction(currentGameState):
     (not reflex agents).
     """
     return currentGameState.getScore()
+
 
 class MultiAgentSearchAgent(Agent):
     """
@@ -101,14 +191,19 @@ class MultiAgentSearchAgent(Agent):
     is another abstract class.
     """
 
-    def __init__(self, evalFn = 'scoreEvaluationFunction', depth = '2'):
-        self.index = 0 # Pacman is always agent index 0
+    def __init__(self, evalFn='scoreEvaluationFunction', depth='2'):
+        self.index = 0  # Pacman is always agent index 0
         self.evaluationFunction = util.lookup(evalFn, globals())
         self.depth = int(depth)
+
 
 class MinimaxAgent(MultiAgentSearchAgent):
     """
     Your minimax agent (question 2)
+
+    Score the terminal state (tree leaves) with self.evaluationFunction.
+    Use self.depth and self.evaluationFunction to generate the miniMax tree
+    and the values in the terminal states.
     """
 
     def getAction(self, gameState):
@@ -128,6 +223,7 @@ class MinimaxAgent(MultiAgentSearchAgent):
         gameState.getNumAgents():
         Returns the total number of agents in the game
 
+        Base cases:
         gameState.isWin():
         Returns whether or not the game state is a winning state
 
@@ -136,6 +232,7 @@ class MinimaxAgent(MultiAgentSearchAgent):
         """
         "*** YOUR CODE HERE ***"
         util.raiseNotDefined()
+
 
 class AlphaBetaAgent(MultiAgentSearchAgent):
     """
@@ -148,6 +245,7 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
         """
         "*** YOUR CODE HERE ***"
         util.raiseNotDefined()
+
 
 class ExpectimaxAgent(MultiAgentSearchAgent):
     """
@@ -164,6 +262,7 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
         "*** YOUR CODE HERE ***"
         util.raiseNotDefined()
 
+
 def betterEvaluationFunction(currentGameState):
     """
     Your extreme ghost-hunting, pellet-nabbing, food-gobbling, unstoppable
@@ -173,6 +272,7 @@ def betterEvaluationFunction(currentGameState):
     """
     "*** YOUR CODE HERE ***"
     util.raiseNotDefined()
+
 
 # Abbreviation
 better = betterEvaluationFunction
